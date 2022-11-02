6ace08c3cb8fe709b2fd6312ecca9b6cd868c747
susmitha <susmithareddychinthala024@gmail.com>
2022-11-02 20:53:46 +0530

    Added: Appending values for Linked List

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -47,9 +47,9 @@ public class LinkedList<T> {
 
         public static void main(String[] args) {
             LinkedList<Integer> numberList = new LinkedList<>();
-            numberList.add(70);
-            numberList.add(30);
             numberList.add(56);
+            numberList.add(30);
+            numberList.add(70);
             numberList.show();
         }
     }
